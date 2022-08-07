0ad64b599326081690cff6fae3ad4f58c65853ce
Orelmis Toribio <orelmistoribio@gmail.com>
2022-08-07 13:50:43 -0400

    Test file

---
+++ src/test/mainTest.java
@@ -0,0 +1,19 @@
+package test;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class mainTest {
+
+    @Test
+    public void test(){
+
+        String test = "testString";
+        assertNotNull(test);
+
+
+    }
+
+
+}
