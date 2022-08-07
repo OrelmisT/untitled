535a90352e4cfaf4932ad921a89f4f74ff7d964a
Orelmis Toribio <orelmistoribio@gmail.com>
2022-08-07 13:28:32 -0400

    "mensaje"

---
+++ src/main.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class main {
+
+    public static void main(String[] args){
+
+        JFrame frame = new JFrame("Sample JFrame");
+        frame.setSize(600,400);
+        frame.setResizable(false);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        System.out.println("Hello world!");
+
+    }
+
+
+}
